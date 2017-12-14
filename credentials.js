0085c8e05a2b8147d98095e2448be596876426c6
tmcgarrah <tdmcg82@yahoo.com>
2017-12-13 19:20:52 -0600

    Delete credentials.js

--- credentials.js
+++
@@ -1,7 +0,0 @@
-const githubAuth = {
-  clientID: '25e0fad0b2cd219b5fca',
-  clientSecret: '474c445375e955e9433c4bf05ccf4e36860ca071',
-  callbackURL: 'http://localhost:3000/login/callback'
-};
-
-module.exports = githubAuth;
